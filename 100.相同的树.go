3ff74a7eb9ebb82b52554525250fd5fc3c1accc0
BarryYan <barry.yansh@gmail.com>
2020-06-12 23:28:10 +0800

    Resolve 100

---
+++ 100.相同的树.go
@@ -0,0 +1,111 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=golang
+ *
+ * [100] 相同的树
+ *
+ * https://leetcode-cn.com/problems/same-tree/description/
+ *
+ * algorithms
+ * Easy (55.53%)
+ * Likes:    374
+ * Dislikes: 0
+ * Total Accepted:    92.8K
+ * Total Submissions: 160.3K
+ * Testcase Example:  '[1,2,3]\n[1,2,3]'
+ *
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   3     2   3
+ *
+ * ⁠       [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入:      1          1
+ * ⁠         /           \
+ * ⁠        2             2
+ *
+ * ⁠       [1,2],     [1,null,2]
+ *
+ * 输出: false
+ *
+ *
+ * 示例 3:
+ *
+ * 输入:       1         1
+ * ⁠         / \       / \
+ * ⁠        2   1     1   2
+ *
+ * ⁠       [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ *
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	tree1 := TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:  2,
+			Left: nil,
+		},
+		Right: &TreeNode{
+			Val:  1,
+			Left: nil,
+		},
+	}
+	tree2 := TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:  1,
+			Left: nil,
+		},
+		Right: &TreeNode{
+			Val:  2,
+			Left: nil,
+		},
+	}
+
+	res := isSameTree(&tree1, &tree2)
+	fmt.Println(res)
+}
+
+// @lc code=start
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p != nil && q == nil ||
+		p == nil && q != nil ||
+		p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+
+// @lc code=end
